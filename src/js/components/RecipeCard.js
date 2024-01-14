6af4b42aa9bf9f7149796e4eef3f5d9cfd2f28e3
Raphael Sanchez <hello@raphaelsanchez.design>
2024-01-15 00:56:30 +0100

    Refactor RecipeCard component to extract createIngredientList function

--- src/js/components/RecipeCard.js
+++ src/js/components/RecipeCard.js
@@ -1,3 +1,19 @@
+const RECIPE_TEXT = "Recette"
+const INGREDIENT_TEXT = "Ingrédient"
+
+const createIngredientList = (ingredients) => {
+  return ingredients
+    .map(
+      ({ ingredient, quantity, unit }) => `
+        <li class="flex flex-col">
+          <span>${ingredient}</span> 
+          <span class="text-neutral-600">${quantity} ${unit || ""}</span>
+        </li>
+      `
+    )
+    .join("")
+}
+
 const RecipeCard = (recipe) => {
   // Destructure recipe object
   const { id, name, description, image, time, ingredients } = recipe
@@ -13,20 +29,11 @@ const RecipeCard = (recipe) => {
       <div class="px-4 py-8">
         <h3 class="font-display text-xl">${name}</h3>
         <p class="absolute top-4 right-4 bg-accent py-2 px-4 rounded-full text-sm">${time} min</p>
-        <h4 class="mt-8 mb-4 uppercase text-neutral-600 text-sm font-bold tracking-widest">Recette</h4>
+        <h4 class="mt-8 mb-4 uppercase text-neutral-600 text-sm font-bold tracking-widest">${RECIPE_TEXT}</h4>
         <p class="line-clamp-4">${description}</p>
-        <h4 class="mt-8 mb-4 uppercase text-neutral-600 text-sm font-bold tracking-widest">Ingrédient</h4>
+        <h4 class="mt-8 mb-4 uppercase text-neutral-600 text-sm font-bold tracking-widest">${INGREDIENT_TEXT}</h4>
         <ul class="grid grid-cols-2 gap-4">
-          ${ingredients
-            .map(
-              ({ ingredient, quantity, unit }) => `
-            <li class="flex flex-col">
-              <span>${ingredient}</span> 
-              <span class="text-neutral-600">${quantity} ${unit || ""}</span>
-            </li>
-          `
-            )
-            .join("")}
+          ${createIngredientList(ingredients)}
         </ul>
       </div>
     </article>
