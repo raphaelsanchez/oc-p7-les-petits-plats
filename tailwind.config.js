0a9b24c5a696a8dd021b810d4527119c0af239b3
Raphael Sanchez <hello@raphaelsanchez.design>
2024-01-15 00:07:10 +0100

    setup Tailwind with css tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      display: ["Anton", "sans-serif"],
+      body: ["Manrope", "sans-serif"],
+    },
+    colors: {
+      accent: "#FFD15B",
+      white: "#FFFFFF",
+      neutral: {
+        300: "#EDEDED",
+        600: "#6e6e6e",
+        900: "#1B1B1B",
+      },
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
