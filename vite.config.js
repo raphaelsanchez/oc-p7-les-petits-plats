90f3e026e30a44474d6b37102a5e3fc7ebfff8e5
Raphael Sanchez <hello@raphaelsanchez.design>
2024-01-28 11:22:29 +0100

    Update BASE_URL in static-deploy.yml and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 // vite.config.js
-import { defineConfig } from "vite"
-
-const isProduction = process.env.NODE_ENV === "production"
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: isProduction ? "/oc-p7-les-petits-plats/" : "/",
+  base: process.env.BASE_URL || '/',
+  // other config options...
 })
